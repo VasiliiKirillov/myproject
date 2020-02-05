f1189a2e9c93608697d880c6a8bcc0cb841b90bf
Vasiily <blend2012@yandex.ru>
2020-02-06 02:59:19 +0300

    Updates

---
+++ precache-manifest.bb56ecf73d980694aff091ddfd6862cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386563a42e4dcb82892f2f17a5fb36e4",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "aa65a1f2c977646dcfcd",
+    "url": "/myproject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "63132dedba35599d3d15",
+    "url": "/myproject/static/js/2.5ce3f19e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myproject/static/js/2.5ce3f19e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa65a1f2c977646dcfcd",
+    "url": "/myproject/static/js/main.0aba74bd.chunk.js"
+  },
+  {
+    "revision": "20693e54987c7c2966cf",
+    "url": "/myproject/static/js/runtime-main.3d8eeb5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
